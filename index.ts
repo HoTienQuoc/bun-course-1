24fbc274d1d3efda3f3729e9935f51195f83c3e8
Your Name <you@example.com>
2023-10-19 18:48:39 +0700

    added index.html

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import figlet from 'figlet'
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+      const url = new URL(req.url)
+      if(url.pathname === '/'){
+        const body = figlet.textSync("Hello world!")
+        return new Response(body)
+      }
+      if(url.pathname === '/about'){
+        return new Response('About me!')
+      }
+      return new Response('404!')
+      
+    },
+  })
+  console.log(`Listening on http://localhost:${server.port} ...`)
+  
